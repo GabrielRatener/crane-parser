ffc733869496d7840947a1bdd908b88ae13fc3b6
Gabe <Gabe@dammit.T-mobile.com>
2017-11-11 19:15:08 -0800

    remove node_modules

--- node_modules/bizubee-compiler/quicktest.js
+++
@@ -1,9 +0,0 @@
-
-import fs from 'fs'
-import {parse} from './src/parser'
-
-const input = process.argv[2];
-const output = process.argv[3];
-const api = parse(fs.readFileSync(input, 'utf8'));
-
-fs.writeFileSync(output, api.getJSText({}), 'utf8');
